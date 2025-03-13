80aec7dca6b33c8f98fd2ac7344f7a3570717706
arash.goharrostami <arash.goharrostami@gmail.com>
2025-03-13 11:01:09 +0330

    Update :: Lib/abstract/abstract.entity.ts

--- libs/abstract/abstract.entity.ts
+++ libs/abstract/abstract.entity.ts
@@ -1,9 +1,16 @@
-
+/** --------------------------------------------------------------------------------------------------------------------
+ * @file abstract.entity.ts
+ * @tutorial only need to extand in your entity.
+ * @description this class will provide mongoDB objectId.
+ */
 import { Entity, Column, ObjectIdColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { ObjectId } from 'mongodb';
 
-
-// TypeORM Entity
+/** --------------------------------------------------------------------------------------------------------------------
+ * @description class AbstractEntity / TypeORM-Entity
+ *
+ * @class AbstractEntity
+ */
 @Entity('User')
 export class AbstractEntity {
   @ObjectIdColumn()
